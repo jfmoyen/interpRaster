bd537490cd80e6bff7364a17b916ff1fe578dc04
Jean-François moyen <moyen@opgc-lmv.lan>
2024-05-28 17:52:02 +0200

    Add contours

--- R/stat-interp-raster.R
+++ R/stat-interp-raster.R
@@ -1,15 +1,45 @@
 
 ##### Raster #####
 StatInterpRaster <- ggproto("StatInterpRaster", ggplot2:::Stat,
-                            compute_group = function(data, scales,linear = TRUE) {
+                            compute_group = function(data, scales,linear = TRUE, rescale=TRUE) {
                               
-                              ii<-akima::interp(x = data$x,
-                                                y = data$y,
+                              xx <- data$x
+                              yy <- data$y
+                              
+                              # If appropriate rescale to 0-1:
+                              if(rescale){
+                                # Scale X
+                                xr <- range(xx,na.rm = T)
+                                xscale <- xr[2] - xr[1]
+                                
+                                xx <- (xx - xr[1]) / xscale
+                                
+                                # Scale Y
+                                yr <- range(yy,na.rm = T)
+                                yscale <- yr[2] - yr[1]
+                                
+                                yy <- (yy - yr[1]) / yscale
+                                
+                              }
+                              
+                              # Interpolation proper
+                              ii<-akima::interp(x = xx,
+                                                y = yy,
                                                 z = data$fill,
                                                 linear=linear)
                               
-                              data.out <- tibble(x = rep(ii$x, nrow(ii$z)),
-                                                 y = rep(ii$y, each = ncol(ii$z)),
+                              xout <- ii$x
+                              yout <- ii$y
+                              
+                              # If rescale, recalculate the true coordinates
+                              if(rescale){
+                                xout <- xout * xscale + xr[1]
+                                yout <- yout * yscale + yr[1]
+                              }
+                              
+                              # Return the data as needed
+                              data.out <- tibble(x = rep(xout, nrow(ii$z)),
+                                                 y = rep(yout, each = ncol(ii$z)),
                                                  fill = as.numeric(ii$z) )
                               
                               return(data.out)
@@ -30,6 +60,7 @@ stat_interp_raster<- function(mapping = NULL, data = NULL, geom = "raster",
   #' @details
   #' The function does not like when the range of X and Y data is too dissimilar...
   #' 
+  #' @param linear (boolean) if TRUE, use linear interpolation. Else use cubic (see ?akima::interp)
   #' @param ... arguments passed to base function,
   #' 
   #' @importFrom akima interp
@@ -39,53 +70,5 @@ stat_interp_raster<- function(mapping = NULL, data = NULL, geom = "raster",
     stat = StatInterpRaster, data = data, mapping = mapping, geom = geom,
     position = position, show.legend = show.legend, inherit.aes = inherit.aes,
     params = list(na.rm = na.rm, ...)
-  )
-}
-
-##### Contour #####
-StatInterpContour <- ggproto("StatInterpRaster", ggplot2:::StatContour,
-                             compute_group = function(data, scales, z.range, bins = NULL, binwidth = NULL, 
-                                                      breaks = NULL, na.rm = FALSE)  {
-                               
-                               ii<-akima::interp(x = data$x,
-                                                 y = data$y,
-                                                 z = data$z)
-                               data <-  tibble(x = rep(ii$x, nrow(ii$z)),
-                                               y = rep(ii$y, each = ncol(ii$z)),
-                                               z = as.numeric(ii$z), 
-                                               group = 1) 
-                               
-                               StatContour$compute_group(data, scales, z.range, 
-                                                         bins, binwidth, breaks, na.rm)
-                               
-                             },
-                             required_aes = c("x", "y", "z")
-)
-
-
-stat_interp_contour<- function(mapping = NULL, data = NULL, geom = "contour",
-                               position = "identity", na.rm = FALSE, 
-                               show.legend = NA,
-                               inherit.aes = TRUE, bins = NULL, binwidth = NULL, 
-                               breaks = NULL,  ...) {
-  #' @title Create a contour geometry from irregularly spaced data
-  #'
-  #' @description
-  #' This stat is based on [geom_contour()],
-  #' and has the same aesthetics, but is able to operate on irregular data
-  #'
-  #' @details
-  #' The function does not like when the range of X and Y data is too dissimilar...
-  #'
-  #' @param ... arguments passed to base function,
-  #' 
-  #' @importFrom akima interp
-  #' @importFrom ggplot2 ggproto layer
-  #' 
-  layer(
-    stat = StatInterpContour, data = data, mapping = mapping, geom = geom,
-    position = position, show.legend = show.legend, inherit.aes = inherit.aes,
-    params = list(na.rm = na.rm, bins = bins, binwidth = binwidth, 
-                  breaks = breaks,  ...)
   )
 }
